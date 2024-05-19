7e0de36c7c42e804b9be3dfdf91739e328f2274d
lng2020 <nanguanlin6@gmail.com>
2024-05-19 22:39:20 +0800

    restruct project

--- main.py
+++ main.py
@@ -64,12 +64,14 @@ strategyTable3 = [
 handA = [range(895, 912), range(1028, 1040), range(744, 762)]
 handB = [range(939, 953), range(1072, 1084), range(789, 803)]
 
+
 def clickz(top_left):
     tagHalfW = int(twidth / 2)
     tagHalfH = int(theight / 2)
     tagCenterX = top_left[0] + tagHalfW
     tagCenterY = top_left[1] + tagHalfH
     pyautogui.click(tagCenterX, tagCenterY, button='left')
+
 
 def poke(h):
     if (h % 13 >= 10 and h % 13 <= 13) or h % 13 == 0:
@@ -79,42 +81,41 @@ def poke(h):
     else:
         return h % 13
 
+
 class App(QWidget):
     def __init__(self):
         super().__init__()
         self.setWindowTitle('Blackjack Bot')
         self.setGeometry(100, 100, 400, 300)
-        
+
         layout = QVBoxLayout()
-        
+
         self.bet_amount_label = QLabel('Bet Amount (in thousands):')
         self.bet_amount_input = QLineEdit()
-        self.bet_amount_map = {'1': 1, '10': 10, '100': 100, '1000': 1000, '10000': 10000}
-        
+        self.bet_amount_map = {'1': 1, '10': 10,
+                               '100': 100, '1000': 1000, '10000': 10000}
+
         self.start_button = QPushButton('Start')
         self.start_button.clicked.connect(self.start_program)
-        
+
         layout.addWidget(self.bet_amount_label)
         layout.addWidget(self.bet_amount_input)
         layout.addWidget(self.start_button)
-        
+
         self.setLayout(layout)
 
     def start_program(self):
         global betAmount, betValue
         betAmount = self.bet_amount_input.text()
         betValue = self.bet_amount_map.get(betAmount)
-        self.run_program()
-    
-    def start_program(self):
-        global betAmount, betValue
-        betAmount = self.bet_amount_input.text()
-        betValue = self.bet_amount_map.get(betAmount)
-        
+
         self.program_thread = ProgramThread()
         self.program_thread.start()
 
-            
+
+max_loc = None
+
+
 class ProgramThread(QThread):
     def __init__(self, parent=None):
         super().__init__(parent)
@@ -123,17 +124,18 @@ class ProgramThread(QThread):
         global max_loc, twidth, theight
         theight, twidth = target.shape[:2]
         tempheight, tempwidth = temp.shape[:2]
-        scaleTemp = resize(temp, (int(tempwidth / screenScale), int(tempheight / screenScale)))
-        
+        scaleTemp = resize(
+            temp, (int(tempwidth / screenScale), int(tempheight / screenScale)))
+
         res = matchTemplate(scaleTemp, target, TM_CCOEFF_NORMED)
         mn_val, max_val, min_loc, max_loc = minMaxLoc(res)
-        
+
         if max_val >= 0.9:
             return 1
         else:
             return 0
-        
-    def handleUnfocused():
+
+    def handleUnfocused(self):
         global max_loc
         temp = imread(r'image/screen.png', 0)
         i = 0
@@ -145,23 +147,13 @@ class ProgramThread(QThread):
                 clickz(top_left)
                 sleep(0.01)
                 return 0
-        if self.compare(doubleexp, temp) == 1:
-            pyautogui.click(130, 1011, button='left')
-            sleep(3)
-            return 0
         pyautogui.click(92, 52, button='left')
 
     def run(self):
         global max_loc, numWins, numLosses, errors, unrecognizable, prevDealerCard, prevHandCard1, prevHandCard2, prevAction, prev2DealerCard, prev2HandCard1, prev2HandCard2, prev2Action
-        hit = imread(r"image/hit.png", 0)
-        double = imread(r"image/double.png", 0)
-        split = imread(r"image/split.png", 0)
-        buyin = imread(r"image/buyin.png", 0)
         win = imread(r"image/win.png", 0)
         lose = imread(r"image/lose.png", 0)
-        unfocused = imread(r"image/unfocused.png", 0)
-        doubleexp = imread(r"image/doubleexp.png", 0)
-        
+
         numWins = 0
         numLosses = 0
         errors = 0
@@ -176,53 +168,53 @@ class ProgramThread(QThread):
         prev2HandCard1 = -1
         prev2HandCard2 = -1
         prev2Action = -1
-        
-        while True:    
+
+        while True:
             while True:
                 stand = imread(r"image/stand.png", 0)
-                bet = imread(r"image/bet" + str(betValue) + "k.png", 0)
-                
-                try:
-                    screenshot('image/screen.png')
-                except:
-                    errors += 1
-                    continue
-                
+                bet = imread(r"image/bet/bet" + str(betValue) + "k.png", 0)
+
+                screenshot('image/screen.png')
                 temp = imread(r'image/screen.png', 0)
-                
+
                 if self.compare(stand, temp) == 1:
                     dealerCard = -1
                     handCard1 = -1
                     handCard2 = -1
                     totalPoints = 0
                     tempheight, tempwidth = temp.shape[:2]
-                    scaleTemp = resize(temp, (int(tempwidth / screenScale), int(tempheight / screenScale)))
-                    
+                    scaleTemp = resize(
+                        temp, (int(tempwidth / screenScale), int(tempheight / screenScale)))
+
                     i = 0
                     while i < 52:
                         i += 1
-                        target = imread(r'image/' + str(i) + '.png', 0)
+                        target = imread(r'image/card/' + str(i) + '.png', 0)
                         theight, twidth = target.shape[:2]
-                        
-                        res = matchTemplate(scaleTemp, target, TM_CCOEFF_NORMED)
+
+                        res = matchTemplate(
+                            scaleTemp, target, TM_CCOEFF_NORMED)
                         mn_val, max_val, min_loc, max_loc = minMaxLoc(res)
-                        
+
                         if max_val >= 0.95:
                             if max_loc[1] < 353 and dealerCard == -1:
-                                x1, y1, x2, y2 = max_loc[0], max_loc[1], max_loc[0] + 15, max_loc[1] + 15
+                                x1, y1, x2, y2 = max_loc[0], max_loc[1], max_loc[0] + \
+                                    15, max_loc[1] + 15
                                 scaleTemp[y1:y2, x1:x2] = 0
                                 dealerCard = poke(i)
                                 prevDealerCard = dealerCard
                                 i -= 1
                             elif (max_loc[0] in [d1 for d2 in handA for d1 in d2]) and handCard1 == -1:
-                                x1, y1, x2, y2 = max_loc[0], max_loc[1], max_loc[0] + 15, max_loc[1] + 15
+                                x1, y1, x2, y2 = max_loc[0], max_loc[1], max_loc[0] + \
+                                    15, max_loc[1] + 15
                                 scaleTemp[y1:y2, x1:x2] = 0
                                 handCard1 = poke(i)
                                 prevHandCard1 = handCard1
                                 totalPoints += handCard1
                                 i -= 1
                             elif (max_loc[0] in [d3 for d4 in handB for d3 in d4]) and handCard2 == -1:
-                                x1, y1, x2, y2 = max_loc[0], max_loc[1], max_loc[0] + 15, max_loc[1] + 15
+                                x1, y1, x2, y2 = max_loc[0], max_loc[1], max_loc[0] + \
+                                    15, max_loc[1] + 15
                                 scaleTemp[y1:y2, x1:x2] = 0
                                 handCard2 = poke(i)
                                 prevHandCard2 = handCard2
@@ -230,10 +222,10 @@ class ProgramThread(QThread):
                                 i -= 1
                         if totalPoints > 20:
                             totalPoints = 20
-                    
+
                     if handCard1 == -1 or handCard2 == -1 or dealerCard == -1:
                         continue
-                    
+
                     if handCard1 == handCard2:  # Two hand cards are equal, use table 3
                         temp = handCard1
                         strategyTable = strategyTable3
@@ -246,7 +238,6 @@ class ProgramThread(QThread):
                     else:  # Other situations, use table 1
                         temp = totalPoints - 3
                         strategyTable = strategyTable1
-                    
                     if strategyTable[temp - 2][dealerCard - 2] == 'S':  # Stand
                         position = (852, 969)
                         clickz(position)
@@ -268,30 +259,27 @@ class ProgramThread(QThread):
                         clickz(position)
                         prevAction = 'P'
                         sleep(0.6)
-                
                 elif self.compare(bet, temp) == 1:
                     top_left = max_loc
                     clickz(top_left)
                     sleep(0.01)
-                
                 elif self.compare(win, temp) == 1:
                     if [prev2DealerCard, prev2HandCard1, prev2HandCard2, prev2Action] == [prevDealerCard, prevHandCard1, prevHandCard2, prevAction]:
                         continue
                     numWins += 1
-                    winRate = numWins / (numLosses + numWins)
                     prev2DealerCard, prev2HandCard1, prev2HandCard2, prev2Action = prevDealerCard, prevHandCard1, prevHandCard2, prevAction
                     sleep(2)
-                
+
                 elif self.compare(lose, temp) == 1:
                     if [prev2DealerCard, prev2HandCard1, prev2HandCard2, prev2Action] == [prevDealerCard, prevHandCard1, prevHandCard2, prevAction]:
                         continue
                     numLosses += 1
-                    winRate = numWins / (numLosses + numWins)
                     prev2DealerCard, prev2HandCard1, prev2HandCard2, prev2Action = prevDealerCard, prevHandCard1, prevHandCard2, prevAction
                     sleep(2)
                 else:
                     sleep(0.2)
             self.handleUnfocused()
+
 
 if __name__ == '__main__':
     app = QApplication([])
